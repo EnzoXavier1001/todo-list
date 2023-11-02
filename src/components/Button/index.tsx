665c68f0a83d75525a433987c1e009ee9b95b4ef
Enzo Costa <enzo_costa7@hotmail.com>
2023-11-02 00:18:19 -0300

    feat: implement click funcionality

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,13 @@
 import style from "./styles.module.css";
 import { PlusCircle } from "@phosphor-icons/react";
 
-export function Button() {
+interface ButtonProps {
+  onClick: () => void;
+}
+
+export function Button({ onClick }: ButtonProps) {
   return (
-    <button className={style.button}>
+    <button className={style.button} onClick={onClick} title="Criar tarefa">
       Criar
       <PlusCircle size={24} />
     </button>

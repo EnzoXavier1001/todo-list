640b71d36f9e9f35c9fb2f0bc0f6683417a70b25
Enzo Costa <enzo_costa7@hotmail.com>
2023-11-02 00:19:56 -0300

    feat: implement list and create your functionalities

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,18 @@
 import style from "./styles.module.css";
+import { ChangeEvent } from "react";
 
-export function Input() {
+interface InputProps {
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+}
+
+export function Input({ onChange, value }: InputProps) {
   return (
-    <input placeholder="Adicione uma nova tarefa" className={style.input} />
+    <input
+      placeholder="Adicione uma nova tarefa"
+      className={style.input}
+      onChange={onChange}
+      value={value}
+    />
   );
 }
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,38 +1,100 @@
-import { Trash } from "@phosphor-icons/react";
+import style from "./styles.module.css";
+import { ChangeEvent } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import { Task } from "../../data/Task";
+
 import { Button } from "../Button";
 import { Input } from "../Input";
-import style from "./styles.module.css";
+import { useState } from "react";
+import { ListItem } from "../ListItem";
 
 export function List() {
+  const [task, setTask] = useState<Task[]>([
+    {
+      id: uuidv4(),
+      text: "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+      done: false,
+    },
+    {
+      id: uuidv4(),
+      text: "Integer urna interdum massa libero ",
+      done: false,
+    },
+  ]);
+
+  const [inputTask, setInputTask] = useState("");
+
+  function handleNewTask(event: ChangeEvent<HTMLInputElement>) {
+    setInputTask(event.target.value);
+  }
+
+  function handleCheckTask(id: string) {
+    const newList = task.map((taskItem) => {
+      if (taskItem.id === id) {
+        taskItem.done = !taskItem.done;
+      }
+      return taskItem;
+    });
+
+    setTask([...newList]);
+  }
+
+  function handleCreateTask() {
+    const newTask = {
+      id: uuidv4(),
+      text: inputTask,
+      done: false,
+    };
+
+    setTask([...task, newTask]);
+    setInputTask("");
+  }
+
+  function handleRemoveTask(id: string) {
+    const newList = task.filter((taskItem) => {
+      return taskItem.id !== id;
+    });
+
+    setTask(newList);
+  }
+
   return (
     <main className={style.main}>
       <header className={style.header}>
-        <Input />
-        <Button />
+        <Input value={inputTask} onChange={(e) => handleNewTask(e)} />
+        <Button onClick={handleCreateTask} />
       </header>
 
       <div className={style.containerList}>
         <header>
           <div className={style.headerContent}>
             <h3 className={style.titleHeaderContent}>Tarefas criadas</h3>
-            <span className={style.counterHeaderContent}>5</span>
+            <span className={style.counterHeaderContent}>{task.length}</span>
           </div>
           <div className={style.headerContent}>
             <h3 className={style.titleHeaderContent}>Concluídas</h3>
-            <span className={style.counterHeaderContent}>2 de 5</span>
+            <span className={style.counterHeaderContent}>
+              {
+                task.filter((teste) => {
+                  return teste.done === true;
+                }).length
+              }{" "}
+              de {task.length}
+            </span>
           </div>
         </header>
         <div className={style.list}>
-          <div className={style.listItem}>
-            <input type="checkbox" />
-            <p>
-              Integer urna interdum massa libero auctor neque turpis turpis
-              semper. Duis vel sed fames integer.
-            </p>
-            <button>
-              <Trash size={32} />
-            </button>
-          </div>
+          {task.map((taskItem) => (
+            <ListItem
+              key={taskItem.id}
+              id={taskItem.id}
+              done={taskItem.done}
+              text={taskItem.text}
+              onRemoveTask={handleRemoveTask}
+              onChecked={handleCheckTask}
+            />
+          ))}
         </div>
       </div>
     </main>

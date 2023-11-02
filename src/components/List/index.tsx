d5c2bde7380ea25eb84b513409a95cc15aa3e7aa
Enzo Costa <enzo_costa7@hotmail.com>
2023-11-01 21:14:43 -0300

    feat: create a header list and start making the list item

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,3 +1,4 @@
+import { Trash } from "@phosphor-icons/react";
 import { Button } from "../Button";
 import { Input } from "../Input";
 import style from "./styles.module.css";
@@ -9,6 +10,31 @@ export function List() {
         <Input />
         <Button />
       </header>
+
+      <div className={style.containerList}>
+        <header>
+          <div className={style.headerContent}>
+            <h3 className={style.titleHeaderContent}>Tarefas criadas</h3>
+            <span className={style.counterHeaderContent}>5</span>
+          </div>
+          <div className={style.headerContent}>
+            <h3 className={style.titleHeaderContent}>Concluídas</h3>
+            <span className={style.counterHeaderContent}>2 de 5</span>
+          </div>
+        </header>
+        <div className={style.list}>
+          <div className={style.listItem}>
+            <input type="checkbox" />
+            <p>
+              Integer urna interdum massa libero auctor neque turpis turpis
+              semper. Duis vel sed fames integer.
+            </p>
+            <button>
+              <Trash size={32} />
+            </button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }

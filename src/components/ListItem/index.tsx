15ba5eb6e27ab82f758daa2136b20294873ac832
Enzo Costa <enzo_costa7@hotmail.com>
2023-11-02 00:16:47 -0300

    feat: create list item

---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,32 @@
+import { Trash } from "@phosphor-icons/react";
+import style from "./styles.module.css";
+import { Task } from "../../data/Task";
+
+interface ListItemProps extends Task {
+  onRemoveTask: (id: string) => void;
+  onChecked: (id: string) => void;
+}
+
+export function ListItem({
+  id,
+  text,
+  onRemoveTask,
+  done,
+  onChecked,
+}: ListItemProps) {
+  return (
+    <div className={style.listItem}>
+      <input type="checkbox" onClick={() => onChecked(id)} />
+      <p className={`${style.listItemText} ${done ? style.hasDone : ""}`}>
+        {text}
+      </p>
+      <button
+        className={style.trashButton}
+        onClick={() => onRemoveTask(id)}
+        title="Deletar Tarefa"
+      >
+        <Trash className={style.trashButtonIcon} size={22} color="#808080" />
+      </button>
+    </div>
+  );
+}
